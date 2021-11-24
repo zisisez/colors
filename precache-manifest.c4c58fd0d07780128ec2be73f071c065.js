5099d198e7323b0f49b34d6570529d4371df805f
Eugene Zolo <68593683+zisisez@users.noreply.github.com>
2021-11-24 14:25:41 +0200

    123

---
+++ precache-manifest.c4c58fd0d07780128ec2be73f071c065.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "23a69ce6a983e1c74159",
+    "url": "/colors/static/css/main.47092313.chunk.css"
+  },
+  {
+    "revision": "23a69ce6a983e1c74159",
+    "url": "/colors/static/js/main.e4f5db3a.chunk.js"
+  },
+  {
+    "revision": "4833ddfbf90387bb4bc8",
+    "url": "/colors/static/js/runtime~main.febe43e6.js"
+  },
+  {
+    "revision": "6cb630f8f4ad8dcfad05",
+    "url": "/colors/static/css/2.c40ac185.chunk.css"
+  },
+  {
+    "revision": "6cb630f8f4ad8dcfad05",
+    "url": "/colors/static/js/2.c439ba46.chunk.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/colors/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "42d40c2d691e237f9fd31753ca9d84d7",
+    "url": "/colors/index.html"
+  }
+];
